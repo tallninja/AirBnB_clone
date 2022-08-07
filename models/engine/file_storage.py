c9212b428e97276195dba87c8e0e7948430505e1
Ernest Wambua <ernestwambua2@gmail.com>
2022-08-07 18:18:54 +0300

    engine

---
+++ models/engine/file_storage.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+
+import json
+import os
+from models.base_model import BaseModel
+
+"""
+models/engine/file_storage.py - file storage engine
+"""
+
+
+class FileStorage:
+    """
+    serializes instances to JSON file and deserializes JSON file to instances
+    """
+
+    __file_path = 'file.json'
+    __objects = {}
+    classes = {"BaseModel": BaseModel}
+
+    def all(self):
+        """returns the dictionary __objects"""
+        return self.__objects
+
+    def new(self, obj):
+        """sets in __objects the obj with key <obj class name>.id"""
+        key = f"{obj.__class__.__name__}.{obj.id}"
+        self.__objects[key] = obj
+
+    def save(self):
+        """serializes __objects to the JSON file"""
+        objects_dict = {}
+        for k, v in self.__objects.items():
+            objects_dict[k] = v.to_dict()
+        with open(self.__file_path, 'w', encoding='utf-8') as json_file:
+            json.dump(objects_dict, json_file)
+
+    def reload(self):
+        """deserializes the JSON file to __objects"""
+        objects_dict = {}
+        if os.path.exists(self.__file_path):
+            with open(self.__file_path, 'r', encoding='utf-8') as json_file:
+                objects_dict = json.load(json_file)
+        for k, v in objects_dict.items():
+            self.__objects[k] = self.classes[v['__class__']](**v)
