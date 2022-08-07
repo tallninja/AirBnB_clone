70c72fd01d2c779569a75907f1e071362e4ed93c
Ernest Wambua <ernestwambua2@gmail.com>
2022-08-07 21:57:37 +0300

    city

---
+++ models/city.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+from models.base_model import BaseModel
+
+"""
+models/city.py - defines a city
+"""
+
+
+class City(BaseModel):
+    """defines a city"""
+
+    state_id = ""
+    name = ""
