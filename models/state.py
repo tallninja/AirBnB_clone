5157d0e5366dc40d051f2535c8502a9014b45575
Ernest Wambua <ernestwambua2@gmail.com>
2022-08-07 21:56:15 +0300

    state

---
+++ models/state.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+from models.base_model import BaseModel
+
+"""
+models/state.py - defines a state
+"""
+
+
+class State(BaseModel):
+    """defines a state"""
+
+    name = ""
