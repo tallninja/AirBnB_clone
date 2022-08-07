c00d5fe390413665c7dadccce0f2416b60fa76b1
Ernest Wambua <ernestwambua2@gmail.com>
2022-08-07 21:58:52 +0300

    amenity

---
+++ models/amenity.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+from models.base_model import BaseModel
+
+"""
+models/amenity.py - defines an amenity
+"""
+
+
+class Amenity(BaseModel):
+    """defines an amenity"""
+
+    name = ""
